5b404c2f4a884638b4e9778cc3c03fbdd2b0874c
Andryyyha <adrianvalir@gmail.com>
2018-05-04 23:58:47 +0400

    Add persistence.xml Add context.xml

--- api/src/main/java/com/csit2111/perfectplaces/config/HibernateJPAConfiguration.java
+++ api/src/main/java/com/csit2111/perfectplaces/config/HibernateJPAConfiguration.java
@@ -1,27 +1,28 @@
 package com.csit2111.perfectplaces.config;
 
+import org.hibernate.jpa.HibernatePersistenceProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
 import javax.sql.DataSource;
 
 @Configuration
-@EnableTransactionManagement
 @PropertySource("classpath:application.properties")
-public class PersistenceContext {
+public class HibernateJPAConfiguration {
 
-    @Autowired
     private Environment environment;
+  //  private LocalContainerEntityManagerFactoryBean entityManagerFactoryBean;
 
 
 
-    @Bean
+    @Bean(name = "dataSource")
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName(environment.getProperty("spring.datasource.driver-class-name"));
@@ -31,12 +32,21 @@ public class PersistenceContext {
     }
 
     @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean() {
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
         LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
-        entityManagerFactoryBean.setDataSource(dataSource());
-        //TODO update PersistenceProvider
-      //  entityManagerFactoryBean.setPersistenceProvider();
         entityManagerFactoryBean.setPackagesToScan("src/main/java/com.csit2111.perfectplcaes/model/");
+        entityManagerFactoryBean.setDataSource(dataSource());
+        entityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        entityManagerFactoryBean.setPersistenceProvider(new HibernatePersistenceProvider());
+        entityManagerFactoryBean.setPersistenceUnitName("test");
         return entityManagerFactoryBean;
+    }
+
+    @Bean(name = "jpaVendorAdapter")
+    public JpaVendorAdapter jpaVendorAdapter()
+    {
+        HibernateJpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
+        jpaVendorAdapter.setDatabasePlatform("org.hibernate.dialect.PostgreSQLDialect");
+        return jpaVendorAdapter;
     }
 }
