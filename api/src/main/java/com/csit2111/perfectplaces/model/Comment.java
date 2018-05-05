8334246912c732e247a314e6576688b27e4cfb95
Andryyyha <adrianvalir@gmail.com>
2018-05-05 22:05:24 +0400

    Enable autoconfiguration via Spring boot

--- api/src/main/java/com/csit2111/perfectplaces/model/Comment.java
+++ api/src/main/java/com/csit2111/perfectplaces/model/Comment.java
@@ -13,15 +13,64 @@ public class Comment {
 
     private String description;
 
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "")
-    private long commentId;
+    @ManyToOne
+    @JoinColumn(name = "comments")
+    private Place place;
 
     private int rateValue;
 
+    //TODO Error:(24, 13) 'One To One' attribute type should not be 'long'
     @OneToOne
-    @JoinColumn(name = "commentId")
+    @JoinColumn(name = "id")
     private long userId;
 
+    public Comment(String description, Place place, int rateValue, long userId) {
+        this.description = description;
+        this.place = place;
+        this.rateValue = rateValue;
+        this.userId = userId;
+    }
 
+    public Comment() {
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Place getPlace() {
+        return place;
+    }
+
+    public void setPlace(Place place) {
+        this.place = place;
+    }
+
+    public int getRateValue() {
+        return rateValue;
+    }
+
+    public void setRateValue(int rateValue) {
+        this.rateValue = rateValue;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
 }
